424b824a313abcdcc729869134e6183c5d50cdc9
iLero07 <tanriverdi.ilero@gmail.com>
2025-10-28 22:38:49 +0300

    Gemini
    Gemini discord bot

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+module.exports = {
+    token: "MTQzMjc0MTYxMDM2MTQ1ODc1OQ.GT7OZ4.thJcpPBsqCAsyABEIat0GJyXRxsuNJaJryUVcs",
+    guildID: "1251493545450410064",
+    prefix: ".",
+    owners: ["986204366073655308"],
+};
